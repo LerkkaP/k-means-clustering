e36c43fe6e3489f5756215ea56d29b2761b28e5c
Erik Peteri <peteri.erik@gmail.com>
2025-07-14 12:15:08 +0300

    Create data structures for data and clusters for 1D example

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <vector>
+
+std::vector<std::vector<double>> kMeansClustering(const int k, const std::vector<double> &data);
 
 int main()
 {
-    std::cout << "Hello world!\n";
+
+    std::vector<double> data = {1.0, 2.0, 3.5, 10.0, 12.0, 11.5};
+    const int k { 2 };
+
+    std::vector<std::vector<double>> clusters { kMeansClustering(k, data) };
+
     return 0;
+}
+
+std::vector<std::vector<double>> kMeansClustering(const int k, const std::vector<double> &data)
+{
+    std::vector<std::vector<double>> clusters = {{10.0}, {12.0}};
+
+    for (double point: data) {
+        std::cout << point;
+    }
+    return clusters;
 }
