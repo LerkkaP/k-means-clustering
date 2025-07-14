a7f3653f0a9fecda88c3610af312bd7e225c8d44
Erik Peteri <peteri.erik@gmail.com>
2025-07-14 13:44:12 +0300

    Add logic for calculating the distance between a point and a cluster

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <limits>
 
 std::vector<std::vector<double>> kMeansClustering(const int k, const std::vector<double> &data);
 
@@ -17,9 +19,23 @@ int main()
 std::vector<std::vector<double>> kMeansClustering(const int k, const std::vector<double> &data)
 {
     std::vector<std::vector<double>> clusters = {{10.0}, {12.0}};
+    int iterations { };
 
-    for (double point: data) {
-        std::cout << point;
+    for (double point : data) {
+        double minDistance = std::numeric_limits<double>::max();
+        int closestCluster { -1 };
+
+        for (int i = 0; i <= 1; ++i) {
+            double distance = abs(point - clusters[i][0]);
+            if (distance < minDistance) {
+                minDistance = distance;
+                closestCluster = i;
+            }
+        } 
+        clusters[closestCluster].push_back(point);
+        std::cout << "The closest cluster for point " << point << " is " << closestCluster << '\n';
+
     }
+    
     return clusters;
 }
